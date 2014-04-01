81494d9ef811be23af11af997dd243306708e8e5
hfcorriez <hfcorriez@gmail.com>
2014-04-01 10:49:13 +0800

    优化文案

--- app/views/index.php
+++ app/views/index.php
@@ -24,7 +24,7 @@ $app-&gt;run();
       <div class="wrap">
         <div class="title">零配置</div>
         <div class="content">
-          无需记住臃肿的配置，没有配置也可运行
+          无需记住臃肿的配置，没有配置也可运行。也可完全基于配置，全依您所爱！
         </div>
       </div>
     </li>
@@ -33,7 +33,7 @@ $app-&gt;run();
       <div class="wrap">
         <div class="title">易扩展</div>
         <div class="content">
-          代码基于中间件模式，只要想到就能做到
+          代码基于中间件模式，掌控输入输出，只要想到就能做到。
         </div>
       </div>
     </li>
@@ -42,7 +42,7 @@ $app-&gt;run();
       <div class="wrap">
         <div class="title">更智能</div>
         <div class="content">
-          写的更少，做的更多
+          写的更少，做的更多。需要时再加载。
         </div>
       </div>
     </li>
@@ -51,7 +51,7 @@ $app-&gt;run();
       <div class="wrap">
         <div class="title">标准化</div>
         <div class="content">
-          代码基于<a href="https://github.com/hfcorriez/fig-standards" target="_blank">PSR规范</a>，有利于社区提交贡献代码
+          代码基于<a href="https://github.com/hfcorriez/fig-standards" target="_blank">PSR规范</a>，代码易读易看。
         </div>
       </div>
     </li>
@@ -60,7 +60,7 @@ $app-&gt;run();
       <div class="wrap">
         <div class="title">高性能</div>
         <div class="content">
-          非C扩展框架中性能突出，性能测试稍后放出
+          非C扩展框架中性能优秀，性能测试稍后放出
         </div>
       </div>
     </li>
@@ -69,7 +69,7 @@ $app-&gt;run();
       <div class="wrap">
         <div class="title">工具化</div>
         <div class="content">
-          有专门的辅助命令行工具：支持生成器；支持模拟Web运行；自带服务器等。正在进行中...
+          强大的命令行工具：自带服务器；生成器快速生成；数据库升级等。
         </div>
       </div>
     </li>
@@ -78,16 +78,16 @@ $app-&gt;run();
       <div class="wrap">
         <div class="title">更现代</div>
         <div class="content">
-          同时支持Web和Cli下开发；支持Restful；支持Yaml/Json/Xml配置；支持依赖注入；事件驱动
+          Web和Cli下开发；Restful；Yaml/Json/Xml配置；依赖注入；事件驱动
         </div>
       </div>
     </li>
 
     <li>
       <div class="wrap">
-        <div class="title">单文件</div>
+        <div class="title">超轻便</div>
         <div class="content">
-          单文件版本，require就能使用，适合快速Web开发。<a href="https://drone.io/github.com/pagon/framework/files/pack/pagon.core.php" target="_blank">前去下载</a>
+          有单文件版本，require就能使用，适合快速Web开发。<a href="https://drone.io/github.com/pagon/framework/files/pack/pagon.core.php" target="_blank">前去下载</a>
         </div>
       </div>
     </li>
@@ -96,7 +96,7 @@ $app-&gt;run();
       <div class="wrap">
         <div class="title">易管理</div>
         <div class="content">
-          使用<a href="http://getcomposer.org" target="_blank">Composer</a>进行依赖管理，安装和管理依赖更简单
+          使用 <a href="http://getcomposer.org" target="_blank">Composer</a> 进行依赖管理，安装和管理依赖库很简单。
         </div>
       </div>
     </li>
